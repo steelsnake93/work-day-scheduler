5c2a76ef7b72d609a3cefae5dc2375fd4e1ca929
steelsnake93 <nikolanikotasev@protonmail.com>
2023-03-01 08:19:01 +0100

    Simplify code and improve user experience in time block creation and editing

--- app/app.js
+++ app/app.js
@@ -1,4 +1,4 @@
-// Define an array of objects to represent each hour of the work day, with properties for the hour, a display time, and an event description
+// Define an array of objects to represent each hour of the work day
 let workDay = [
     { hour: 9, display: "9AM", event: "", },
     { hour: 10, display: "10AM", event: "", },
@@ -14,22 +14,22 @@ let workDay = [
 function displayCurrentTime() {
    let currentDate = moment().format("dddd, MMMM Do");
    let currentTime = moment().format("HH:mm:ss");
-   $("#currentDay").text(currentDate + " " + currentTime);
+   $("#currentDay").text(`${currentDate} ${currentTime}`);
 }
-setInterval(displayCurrentTime, 1000);
+setInterval(displayCurrentTime, 1000); // Update current time every second
 // Function to create time blocks
 function createTimeBlocks() {
     let currentHour = moment().hour();
-    // for loop to create a row for each hour of the work day
-    for (let i = 0; i < workDay.length; i++) {
+    // Loop over each hour of the work day and create a time block
+    for (let hour of workDay) {
         let timeBlock = $("<div>").addClass("row time-block");
-        let hourEl = $("<div>").addClass("col-md-1 hour").text(workDay[i].display);
-        let eventEl = $("<textarea>").addClass("col-md-10 description").val(workDay[i].event);
+        let hourEl = $("<div>").addClass("col-md-1 hour").text(hour.display);
+        let eventEl = $("<textarea>").addClass("col-md-10 description").val(localStorage.getItem(hour.display));
         let saveBtn = $("<button>").addClass("col-md-1 saveBtn").html("<i class='fas fa-save'></i>");
         // Adds classes to the event column based on whether the hour is in the past, present, or future
-        if(workDay[i].hour < currentHour) {
+        if(hour.hour < currentHour) {
             eventEl.addClass("past");
-        }else if(workDay[i].hour === currentHour) {
+        }else if(hour.hour === currentHour) {
             eventEl.addClass("present");
         }else{
             eventEl.addClass("future");
@@ -39,7 +39,6 @@ function createTimeBlocks() {
         $(".container").append(timeBlock);
     }
 }
-createTimeBlocks();
 // Function to change color based on past present and future
 function changeColor() {
     let currentHour = moment().hour();
@@ -54,8 +53,6 @@ function changeColor() {
         }
     });
 }
-changeColor();
-// Function to allow user to enter an event
 // Function to save event in local storage on button click
 function saveEvent() {
     $(".saveBtn").on("click", function() {
@@ -65,5 +62,16 @@ function saveEvent() {
     });
 }
 // Function to display saved events from local storage
+function displaySavedEvents() {
+    $(".description").each(function() {
+        let hour = $(this).parent().find(".hour").text();
+        let event = localStorage.getItem(hour);
+        $(this).val(event);
+    });
+}
 // Call the functions
+displayCurrentTime();
+createTimeBlocks();
+changeColor();
+displaySavedEvents();
 saveEvent();
